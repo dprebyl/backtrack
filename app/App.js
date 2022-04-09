14a16f5b4b0b1c3c8b2e98a8da77b5b21bf26a97
Bikash Kalikote <b704k889@ku.edu>
2022-04-09 11:33:46 -0500

    testing hello world

--- app/App.js
+++ app/App.js
@@ -1,11 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app! </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -19,3 +18,25 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+/*
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const HelloWorldApp = () => {
+  return (
+    <View style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+
+      }}>
+      <Text>Testing 1 </Text>
+      <Text>Testing 2 </Text>
+    </View>
+  );
+}
+
+export default HelloWorldApp;
+*/
