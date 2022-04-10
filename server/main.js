1a160762345f809ac4aa1d4c14be67d67d2f67cf
Drake Prebyl <drakeprebyl@gmail.com>
2022-04-09 23:45:03 -0500

    Send Twilio SMS check and emergency messages

--- server/main.js
+++ server/main.js
@@ -25,8 +25,11 @@ const db_date = d => d.toISOString().slice(0, 19).replace('T', ' ');
 const app = express();
 const port = 3000;
 
+// Ability to receive post requests
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+// Test endpoints ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 // Test homepage
 app.get("/", (req, res) => {
@@ -60,6 +63,8 @@ app.post("/text", (req, res) => {
 	res.send(`Texting "${msg}" to ${num}. <a href="/">Back</a>`);
 });
 
+// App endpoints ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 // App endpoint to retur a list all alerts set up by this user
 app.get("/get-alerts", (req, res) => {
 	console.log("Get alerts", req.query);
@@ -80,7 +85,8 @@ app.post("/clear-alert", (req, res) => {
 		[req.body.user_phone, req.body.alert_time]);
 });
 
-// Periodically check for alerts to send out
+// Periodic checks ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 setInterval(() => {
 	let now = db_date(new Date()); // Appears to be in UTC
 	console.log(now);
@@ -88,14 +94,32 @@ setInterval(() => {
 	let alerts = db_query("SELECT user_phone, check_time FROM alerts WHERE status = 1 AND check_time < ?", [now]);
 	for (let alert of alerts) {
 		console.log("user phone", alert);
-		// TODO twilio
+		twilio.messages.create({
+			body: "Are you home yet? Your emergency contact is about to be notified. Open the BackTrack app to delay or disarm.",
+			from: twilio_credentials.phone_number,
+			to: alert.user_phone
+		}).then(message => {
+			console.log("twilio SMS sent", message);
+			// Only update the status if the message was sent successfully
+			db_query("UPDATE alerts SET status = 2 WHERE user_phone = ? AND check_time = ?",
+				[alert.user_phone, alert.check_time]);
+		});
 	}
 	
 	// Emergency contacts to contact
-	alerts = db_query("SELECT alert_message, contact_phone FROM alerts WHERE status BETWEEN 1 AND 2 AND alert_time < ?", [now]);
+	alerts = db_query("SELECT user_phone, contact_phone, alert_time, alert_message FROM alerts WHERE status BETWEEN 1 AND 2 AND alert_time < ?", [now]);
 	for (let alert of alerts) {
 		console.log("contact phone", alert);
-		// TODO twilio
+		twilio.messages.create({
+			body: `!BACKTRACK ALERT! The person with the phone number ${alert.user_phone} has not made it where they expected to in time. They provided the following message: "${alert.alert_message}". Please check to ensure they are safe.`,
+			from: twilio_credentials.phone_number,
+			to: alert.user_phone
+		}).then(message => {
+			console.log("twilio SMS sent", message);
+			// Only update the status if the message was sent successfully
+			db_query("UPDATE alerts SET status = 3 WHERE user_phone = ? AND alert_time = ?",
+				[alert.user_phone, alert.alert_time]);
+		});
 	}
 }, 60*1000);
 
