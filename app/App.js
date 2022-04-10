4ce8b0b762de9ef817c2f270c848ca7434e6705c
James Hanselman <j729h566@home.ku.edu>
2022-04-10 07:06:52 -0500

    Merge branch 'James'

--- app/App.js
+++ app/App.js
@@ -1,23 +1,67 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 import Time from './Components/Time';
 import Header from './Components/Header';
 import Alerts from './Components/Alerts';
-import {getLocation} from './location-service.js';
+import {getLocation,isHome,haversine} from './location-service.js';
 import {createAlert,getAlerts,clearAlert} from './Request';
+import { requestForegroundPermissionsAsync } from 'expo-location';
 
 
 //app it self
 const App = () =>{
   const [long, setLong] = useState("");
   const [lat, setLat] = useState("");
+  const [alerts,setAlerts] = useState(["Alert 1","Alert 2"]);
+  const [userPhone, setUserPhone] = useState("+13149205055");
+  const [contactPhone,setContactPhone] = useState("+13149205055"); 
   const [address, setAdress] = useState('1234 N 123 St Number City Number'); //stores address, use address to access it
   const [time, setTime] = useState("11:11");
+  const [locString, setLocString] = useState("");
+  const [homeLong, setHomeLong] = useState(-95.25036368196945);
+  const [homeLat, setHomeLat] = useState(38.965795247022086);
+  const [buttonColor, setColor] = useState("#99ccff");
+  const [radius, setRadius] = useState("0.05 km"); //50 meters   
+  
+  const checkAlertTrigger = async () => {
+    let currentDate = new Date();
 
-  const addAlert = () => {
-    
-    console.log("addAlertCalled");
+    //every 55 check if alert goes out
+    for(let i = 0; i < alertCache.length; i++) {
+      if(alertCache[i].alert_time <= currentDate) {
+
+      }
+    }
   }
+
+  const buttonHandler = () => {
+    console.log(radius);
+    isHome(homeLong,homeLat,long,lat,radius)
+    if(isHome(homeLong,homeLat,long,lat,radius) == true) {
+      console.log("blue");
+      setColor("#3366ff");
+    } else {
+      console.log("red");
+      setColor("#ff3300");
+    }
+  }
+
+  useEffect(() => {
+      (async () => {
+        try {
+          let location = await getLocation()
+          let long = location.coords.longitude;
+          let lat  = location.coords.latitude;
+
+          setLocString("Current Location: " + lat + ", " + long + "\nHome Location: " + homeLat + ", " + homeLong + 
+                      "\nYou are " + haversine(lat,long,homeLat,homeLong) + " km from home");
+                      setLong(long);
+                      setLat(lat);
+        } catch (err) {
+          setLong(err.message)
+        }
+      })();
+  },[]);
 
   return(
     <View style={styles.container}>
@@ -29,6 +73,16 @@ const App = () =>{
        onChangeText={(getAddress) => setAdress(getAddress)} /> 
       <Time/>
       <Alerts/>
+      <Text>Radius:</Text>
+      <TextInput
+       placeholder={radius}
+       onChangeText={(getRadius) => setRadius(getRadius)} /> 
+      <Button 
+        title="Am I Home?"
+        onPress={buttonHandler}
+        color={buttonColor}
+      />
+      <Text>{locString}</Text>
     </View>
   );
 };
--- app/location-service.js
+++ app/location-service.js
@@ -17,21 +17,45 @@ export const getLocation = async () => {
     }
 }
 
-export const isHome = async (homeLong, homeLat, radius) => {
+export const isHome = async (homeLong, homeLat, currentLong,currentLat, radius) => {
     try {
         //get current location and compare to home location
-        let currentLoc = getLocation();
-        let distanceToHome = haversine(currentLoc.coords.latitude,currentLoc.coords.longitude,homeLat,homeLong);
-        if(Math.abs(distanceToHome) <= radius ) return true;
-        else return false;
+        let distanceToHome = haversine(currentLat,currentLong,homeLat,homeLong);
+        console.log("distance to home: ",distanceToHome,"radius",radius);
+        if(Math.abs(distanceToHome) <= radius ) {
+            console.log("returngin true") 
+            return true;}
+        else {
+            console.log("returning false");
+            return false;
+            
+        } 
     } catch (err) {
-
+        console.log(err.message);
     }
 }
 
-const haversine = (lat1, long1,lat2,long2) => {
-    const r = 3485000;
-    let d = 2*r*Math.asin(Math.sqrt(Math.pow(Math.sin((lat2-lat1)/2),2) + Math.cos(lat1)*Math.cos(lat2)*Math.pow(Math.sin((long2-long1)/2),2)));
-    console.log(d);
-    return d;
+const toRad = (val) => {
+    val = val*Math.PI/180;
+}
+
+export const haversine = (lat1, lon1, lat2, lon2) => {
+    // distance between latitudes
+    // and longitudes
+    let dLat = (lat2 - lat1) * Math.PI / 180.0;
+    let dLon = (lon2 - lon1) * Math.PI / 180.0;
+       
+    // convert to radiansa
+    lat1 = (lat1) * Math.PI / 180.0;
+    lat2 = (lat2) * Math.PI / 180.0;
+     
+    // apply formulae
+    let a = Math.pow(Math.sin(dLat / 2), 2) +
+               Math.pow(Math.sin(dLon / 2), 2) *
+               Math.cos(lat1) *
+               Math.cos(lat2);
+    let rad = 6371;
+    let c = 2 * Math.asin(Math.sqrt(a));
+    return rad * c;
+     
 }
